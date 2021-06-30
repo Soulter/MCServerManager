15e7def6e5bd5915e6985b7864ea05c48514c4ef
Soulter <5250784+soulter@user.noreply.gitee.com>
2021-06-30 19:24:47 +0800

    新增了玩家头像预览

--- app/src/main/java/com/soulter/mcservermanager/PlayerListAdapter.java
+++ app/src/main/java/com/soulter/mcservermanager/PlayerListAdapter.java
@@ -1,6 +1,8 @@
 package com.soulter.mcservermanager;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,16 +16,27 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import main.conn.MinecraftServer;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class PlayerListAdapter extends ArrayAdapter<List<String>> {
 
 
     private int resourceId;
     private List<List<String>> playersList = new ArrayList<>();
+    private Map<Integer,Bitmap> avatarsMap = new HashMap<>();
 
     public PlayerListAdapter(Context context, int itemResId , List<List<String>> list ){
         super(context, itemResId, list);
@@ -34,9 +47,9 @@ public class PlayerListAdapter extends ArrayAdapter<List<String>> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
         View view;
         PlayerListAdapter.ViewHolder viewHolder;
-        if (convertView==null){
 
             // 避免ListView每次滚动时都要重新加载布局，以提高运行效率
             view= LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
@@ -47,17 +60,95 @@ public class PlayerListAdapter extends ArrayAdapter<List<String>> {
             viewHolder.playerName.setTypeface(viewHolder.playerName.getTypeface(), Typeface.BOLD);
             viewHolder.playerUuid=view.findViewById(R.id.item_player_uuid);
             viewHolder.playerUuid.setTypeface(viewHolder.playerUuid.getTypeface(), Typeface.BOLD);
+            viewHolder.playerAvatar=view.findViewById(R.id.item_player_avatar);
+            viewHolder.avatarLoadBar=view.findViewById(R.id.item_avatar_loading);
+
 
             // 将ViewHolder存储在View中（即将控件的实例存储在其中）
             view.setTag(viewHolder);
-        } else{
-            view=convertView;
-            viewHolder=(PlayerListAdapter.ViewHolder) view.getTag();
-        }
 
-        // 获取控件实例，并调用set...方法使其显示出来
-        viewHolder.playerName.setText(playersList.get(position).get(0));
-        viewHolder.playerUuid.setText(playersList.get(position).get(1));
+
+
+            // 获取控件实例，并调用set...方法使其显示出来
+            viewHolder.playerName.setText(playersList.get(position).get(0));
+            viewHolder.playerUuid.setText(playersList.get(position).get(1));
+
+            viewHolder.avatarLoadBar.setVisibility(View.VISIBLE);
+            if (avatarsMap.containsKey(position)){
+                viewHolder.avatarLoadBar.setVisibility(View.GONE);
+                viewHolder.playerAvatar.setVisibility(View.VISIBLE);
+                viewHolder.playerAvatar.setImageBitmap(avatarsMap.get(position));
+
+            } else{
+                String avatarUrl = "https://crafthead.net/avatar/"+playersList.get(position).get(1);
+                Log.v("lwl","avatarUrl:"+avatarUrl);
+                Request request = new Request.Builder()
+                        .url(avatarUrl)
+                        .build();
+                OkHttpClient okHttpClient =  new OkHttpClient();
+                okHttpClient.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Log.d("lwl", "onFailure: " + e.getMessage());
+                        viewHolder.avatarLoadBar.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                viewHolder.avatarLoadBar.setVisibility(View.GONE);
+                            }
+                        });
+                        viewHolder.playerAvatar.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                viewHolder.playerAvatar.setVisibility(View.VISIBLE);
+                                viewHolder.playerAvatar.setImageResource(R.drawable.steve);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        try{
+                            byte[] picture_bt = response.body().bytes();
+                            Bitmap bitmap = BitmapFactory.decodeByteArray(picture_bt, 0, picture_bt.length);;
+                            viewHolder.avatarLoadBar.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    viewHolder.avatarLoadBar.setVisibility(View.GONE);
+                                }
+                            });
+                            viewHolder.playerAvatar.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Log.v("lwl","setBitmap:"+bitmap);
+                                    viewHolder.playerAvatar.setVisibility(View.VISIBLE);
+                                    viewHolder.playerAvatar.setImageBitmap(bitmap);
+                                    avatarsMap.put(position,bitmap);
+
+                                }
+                            });
+
+                        }catch (Exception e){
+                            e.printStackTrace();
+                            viewHolder.avatarLoadBar.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    viewHolder.avatarLoadBar.setVisibility(View.GONE);
+                                }
+                            });
+                            viewHolder.playerAvatar.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    viewHolder.playerAvatar.setVisibility(View.VISIBLE);
+                                    viewHolder.playerAvatar.setImageResource(R.drawable.steve);
+                                }
+                            });
+
+                        }
+
+                    }
+                });
+            }
+
 
 
         return view;
@@ -68,5 +159,7 @@ public class PlayerListAdapter extends ArrayAdapter<List<String>> {
     class ViewHolder{
         TextView playerName;
         TextView playerUuid;
+        ImageView playerAvatar;
+        ProgressBar avatarLoadBar;
     }
 }
